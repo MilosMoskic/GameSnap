aa138cfdf156c8bfbeedc990a5997b930d4823ba
Milos Moskic <milosmoskic147@gmail.com>
2024-12-19 16:13:04 +0100

    Created client-side application

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a28bd996824689acc4896a92cabe951ef62465db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12bee36507fb39b3fccdae85d9d9c18619c0e60d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
